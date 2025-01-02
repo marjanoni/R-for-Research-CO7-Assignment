4f65ae6fedf9766992a49054c4f18247c08398a3
marjanoni <kmmarjan18@gmail.com>
2025-01-03 03:54:03 +0600

    This folder contain the data and the script to determine The Factors associated with the level of knowledge among parents of school-going children.

---
+++ table 4/script/Ordinal logistic regression for table 4.R
@@ -0,0 +1,35 @@
+library(tidyverse)
+library(MASS)
+library(gtsummary)
+library(readxl)
+library(XLS)
+library(gt)
+
+library(readr)
+cleandata <- read_csv("clean data/cleandata.csv")
+
+
+# change column name for easier analysis
+amr_clean1 <- cleandata |>
+  select(1:11, 46)
+colnames(amr_clean1) <- paste0("Q", 1:12)
+
+#change dependent variable (string) into a factor mode
+str(amr_clean1$Q12)
+amr_clean1$Q12 <- factor(amr_clean1$Q12, ordered = TRUE)
+
+#ordinary logistical regression model
+
+OLR <- polr(Q12 ~ Q1+Q2+Q3+Q4+Q5+Q6+Q7+Q8+Q9+Q10+Q11 , amr_clean1, Hess = TRUE)
+
+#table summary
+tbl_regression(OLR, exponentiate = TRUE) |>
+  add_global_p() |>
+  bold_labels() |>
+  bold_p(t = 0.05) |>
+  as_gt() |>
+  gtsave(OLR, "data\table_4")
+  
+  
+ 
+  
