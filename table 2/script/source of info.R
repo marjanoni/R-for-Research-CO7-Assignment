c7a4a9c99c77e93c594b5759f580c7e5211c980f
marjanoni <kmmarjan18@gmail.com>
2025-01-04 23:01:58 +0600

    data and script for table 2

--- table 2/script/source of info.R
+++ table 2/script/source of info.R
@@ -3,8 +3,11 @@ library(gtsummary)
 library(gt)
 library(readxl)
 infodata <- read_excel("raw_data/AMR_KAP_Data.xlsx")
+# selecting necessary columns for analysis
+
 infodata <- infodata |>
   select(41:49)
+#table summary for the demographics
 
 tbl_summary(infodata) |>
 modify_footnote(c(all_stat_cols()) ~ NA) |> #remove footnotes
