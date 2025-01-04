8e5ff49226173f9c92fc17e33ba91a4dd6c4f0b1
marjanoni <kmmarjan18@gmail.com>
2025-01-04 21:59:00 +0600

    table 4 script

--- table 4/script/Ordinal logistic regression for table 4.R
+++ table 4/script/Ordinal logistic regression for table 4.R
@@ -11,16 +11,16 @@ cleandata <- read_csv("clean data/cleandata.csv")
 
 # change column name for easier analysis
 amr_clean1 <- cleandata |>
-  select(1:11, 46)
-colnames(amr_clean1) <- paste0("Q", 1:12)
+  select(1:9, 46)
+
 
 #change dependent variable (string) into a factor mode
-str(amr_clean1$Q12)
-amr_clean1$Q12 <- factor(amr_clean1$Q12, ordered = TRUE)
+str(amr_clean1$Knowledge_Status)
+amr_clean1$Knowledge_Status <- factor(amr_clean1$Knowledge_Status, ordered = TRUE)
 
 #ordinary logistical regression model
 
-OLR <- polr(Q12 ~ Q1+Q2+Q3+Q4+Q5+Q6+Q7+Q8+Q9+Q10+Q11 , amr_clean1, Hess = TRUE)
+OLR <- polr(Knowledge_Status ~ . , amr_clean1, Hess = TRUE)
 
 #table summary
 tbl_regression(OLR, exponentiate = TRUE) |>
@@ -28,7 +28,7 @@ tbl_regression(OLR, exponentiate = TRUE) |>
   bold_labels() |>
   bold_p(t = 0.05) |>
   as_gt() |>
-  gtsave("table_4.docx")
+  gtsave("table 4/table/table 4.docx")
   
   
  
